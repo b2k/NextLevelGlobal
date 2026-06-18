071ca8c4ee8741c1c0dc010e123b2d6946d66108
Brian Marquis <brian@quotepro.com>
2026-06-17 21:00:13 -0500

    Adding scripture summaries for summer 200

--- src/lib/config/pages/groups/men/200/summer/page.ts
+++ src/lib/config/pages/groups/men/200/summer/page.ts
@@ -11,35 +11,58 @@ export const page: GroupPage = {
 	heroTextColor: 'white',
 	theme: 'dark',
 	sections: [
-	{
-		id: 'summer-description',
-		title: 'Recommended Summer Reading & Resources',
-		description: 'During the summer there are no formal groups, but we encourage men to continue daily Bible reading and prayer. The calendar provides a reading schedule to help you read the Bible using our 4 year reading plan.',
-		theme: 'dark'
-	},
-	{
-		id: 'additional-resources',
-		title: 'Additional Resources',
-		theme: 'light',
-		items: [
-			{
-				type: 'pdf',
-				title: 'Next Level Vision',
-				pdf: '/groups/men/vision.pdf'
-			},
-			{
-				type: 'pdf',
-				title: '200 Growth Plan',
-				pdf: '/groups/men/200/growth-plan.pdf'
-			},
-			{
-				type: 'pdf',
-				title: 'Commitment',
-				pdf: '/groups/men/commitment.pdf'
-			}
-		]
-	}
-],
-	calendar,
-
+		{
+			id: 'summer-description',
+			title: 'Recommended Summer Reading & Resources',
+			description:
+				'During the summer there are no formal groups, but we encourage men to continue daily Bible reading and prayer. The calendar provides a reading schedule to help you read the Bible using our 4 year reading plan.',
+			theme: 'dark'
+		},
+		{
+			id: 'scripture-summaries',
+			title: 'Scripture Summaries & Questions',
+			subtitle: 'Download weekly summaries and discussion questions.',
+			theme: 'light',
+			items: [
+				{
+					type: 'pdf',
+					title: 'Weeks 67-69',
+					pdf: '/groups/men/200/summer/nlgm-summer200-w67-69.pdf'
+				},
+				{
+					type: 'pdf',
+					title: 'Weeks 70-72',
+					pdf: '/groups/men/200/summer/nlgm-summer200-w70-72.pdf'
+				},
+				{
+					type: 'pdf',
+					title: 'Weeks 73-76',
+					pdf: '/groups/men/200/summer/nlgm-summer200-w73-76.pdf'
+				}
+			]
+		},
+		{
+			id: 'additional-resources',
+			title: 'Additional Resources',
+			theme: 'light',
+			items: [
+				{
+					type: 'pdf',
+					title: 'Next Level Vision',
+					pdf: '/groups/men/vision.pdf'
+				},
+				{
+					type: 'pdf',
+					title: '200 Growth Plan',
+					pdf: '/groups/men/200/growth-plan.pdf'
+				},
+				{
+					type: 'pdf',
+					title: 'Commitment',
+					pdf: '/groups/men/commitment.pdf'
+				}
+			]
+		}
+	],
+	calendar
 };
